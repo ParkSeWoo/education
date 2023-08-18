791ea28b97a111b2b69a313703d52e6a7743c075
ParkSeWoo <sewooimda@nate.com>
2023-08-18 04:40:06 +0000

    init
    파이썬 강좌 정리

---
+++ python/default/main.py
@@ -0,0 +1,25 @@
+# print("Hello")
+
+# print("Hello" + " " + "World")
+
+# print({'name': 'park', 'age': 42})
+
+# print([1, 2, 3])
+
+# print((1, 2, 3))
+
+# print(42)
+# print(3.14)
+# print(True)
+
+# print('home', 'user', 'documents', sep='/')
+
+#Incorrect format
+# print("What is wrong here) " 빠짐
+# print(("Something wrong here") ) 빠짐
+
+print(input("What is your age? "))
+
+print("My name is " + input("How are you? What is your name?"))
+
+print(len(input("what is yuour name?")))
